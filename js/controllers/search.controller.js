0760f6719523c6e2bac01db1d16f27f4223562a7
angel fabian silvera <108084824+fabiansilvera@users.noreply.github.com>
2022-11-16 21:25:53 -0300

    Update search.controller.js

--- js/controllers/search.controller.js
+++ js/controllers/search.controller.js
@@ -25,10 +25,10 @@ const crearLineaProducto = (url,nombre,precio,id) => {
     const linea = document.createElement("div");
     linea.classList.add("box--product");
     const contenido =
-    `<a href="AluraGeek/screens/producto.html?id=${id}"><img class="product--img" src="../assets/img/${url}.png" alt="producto"></a>
+    `<a href="producto.html?id=${id}"><img class="product--img" src="../assets/img/${url}.png" alt="producto"></a>
     <p class="product--text">${nombre}</p>
     <p class="product--price">$${precio}</p>
-    <a class="product--enlace" href="AluraGeek/screens/producto.html?id=${id}">Ver Producto</a>`;
+    <a class="product--enlace" href="producto.html?id=${id}">Ver Producto</a>`;
     linea.innerHTML = contenido;
 
     return linea;
