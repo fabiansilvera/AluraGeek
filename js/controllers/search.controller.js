36c6365c97a56ce5de3aec88e4b4dd49786c5164
angel fabian silvera <108084824+fabiansilvera@users.noreply.github.com>
2022-11-16 21:05:44 -0300

    Add files via upload

--- js/controllers/search.controller.js
+++ js/controllers/search.controller.js
@@ -4,6 +4,10 @@ const table = document.querySelector("[data-table]");
 const obetenerInformacion = () => {
     const url = new URL(window.location);
     const search = url.searchParams.get("search");
+
+    if(search == "" || !search) {
+        window.location.href = "../index.html"
+    }
     
     productServices.listaProductos().then( (data) => {
         data.forEach(({url,categoria,nombre,precio,id}) => {
@@ -21,10 +25,10 @@ const crearLineaProducto = (url,nombre,precio,id) => {
     const linea = document.createElement("div");
     linea.classList.add("box--product");
     const contenido =
-    `<a href="../producto.html?id=${id}"><img class="product--img" src="../assets/img/${url}.png" alt="producto"></a>
+    `<a href="producto.html?id=${id}"><img class="product--img" src="../assets/img/${url}.png" alt="producto"></a>
     <p class="product--text">${nombre}</p>
     <p class="product--price">$${precio}</p>
-    <a class="product--enlace" href="../producto.html?id=${id}"">Ver Producto</a>`;
+    <a class="product--enlace" href="producto.html?id=${id}"">Ver Producto</a>`;
     linea.innerHTML = contenido;
 
     return linea;
